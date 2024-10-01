8ea85548df75938cc53a066fce323b0939c1f325
table-harmony <lironkaner2007@gmail.com>
2024-10-01 12:36:22 +0300

    soap web service

--- app.js
+++ app.js
@@ -1,115 +1,55 @@
 const express = require("express");
 const cors = require("cors");
+const soap = require("soap");
+const bodyParser = require("body-parser");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(
-  cors({
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+  bodyParser.raw({
+    type: function () {
+      return true;
+    },
+    limit: "5mb",
   })
 );
 
-// Root endpoint
-app.get("/", (req, res) => {
-  res.json({
-    service: "ASPHarmony",
-    version: "1.0.0",
-    endpoints: [
-      { path: "/", method: "GET", description: "Service information" },
-      { path: "/api", method: "GET", description: "API root" },
-    ],
-  });
-});
-
-// API endpoint
-app.get("/api", (req, res) => {
-  res.json({ message: "Welcome to the ASPHarmony API" });
-});
-
-// Service metadata endpoint
-app.get("/$metadata", (req, res) => {
-  if (req.accepts("json")) {
-    res.json({
-      serviceName: "ASPHarmony",
-      version: "1.0.0",
-      description: "A simple API service for ASPHarmony",
-      endpoints: [
-        {
-          path: "/",
-          method: "GET",
-          description: "Service information",
-          responseFormat: {
-            service: "string",
-            version: "string",
-            endpoints: "array",
-          },
-        },
-        {
-          path: "/api",
-          method: "GET",
-          description: "API root",
-          responseFormat: {
-            message: "string",
-          },
-        },
-      ],
-      dataTypes: {},
-      authenticationMethod: "None",
-      contactInformation: {
-        developer: "TableHarmony",
-        email: "contact@tableharmony.com",
+// SOAP service
+const serviceObject = {
+  ASPHarmonyService: {
+    ASPHarmonyPort: {
+      GetServiceInfo: function (args) {
+        return {
+          Service: "ASPHarmony",
+          Version: "1.0.0",
+        };
       },
-    });
-  } else {
+      AddNumbers: function (args) {
+        const result = args.a + args.b;
+        return { AddResult: result };
+      },
+    },
+  },
+};
+
+// WSDL definition
+const xml = require("fs").readFileSync("service.wsdl", "utf8");
+
+// Create SOAP server
+soap.listen(app, "/service", serviceObject, xml);
+
+// Serve WSDL file
+app.get("/service", function (req, res) {
+  if (req.query.wsdl !== undefined) {
     res.type("application/xml");
-    res.send(`<?xml version="1.0" encoding="UTF-8"?>
-<edmx:Edmx xmlns:edmx="http://docs.oasis-open.org/odata/ns/edmx" Version="4.0">
-  <edmx:DataServices>
-    <Schema xmlns="http://docs.oasis-open.org/odata/ns/edm" Namespace="ASPHarmony.Models">
-      <EntityContainer Name="ASPHarmonyService">
-        <EntitySet Name="ServiceInfo" EntityType="ASPHarmony.Models.ServiceInfo">
-          <Annotation Term="OData.PublishingService">
-            <Record>
-              <PropertyValue Property="Name" String="ASPHarmony" />
-              <PropertyValue Property="Version" String="1.0.0" />
-            </Record>
-          </Annotation>
-        </EntitySet>
-        <FunctionImport Name="GetServiceInfo" ReturnType="ASPHarmony.Models.ServiceInfo" />
-        <FunctionImport Name="GetApiRoot" ReturnType="ASPHarmony.Models.ApiRoot" />
-      </EntityContainer>
-      <EntityType Name="ServiceInfo">
-        <Property Name="Service" Type="Edm.String" />
-        <Property Name="Version" Type="Edm.String" />
-        <Property Name="Endpoints" Type="Collection(ASPHarmony.Models.Endpoint)" />
-      </EntityType>
-      <EntityType Name="Endpoint">
-        <Property Name="Path" Type="Edm.String" />
-        <Property Name="Method" Type="Edm.String" />
-        <Property Name="Description" Type="Edm.String" />
-      </EntityType>
-      <EntityType Name="ApiRoot">
-        <Property Name="Message" Type="Edm.String" />
-      </EntityType>
-    </Schema>
-  </edmx:DataServices>
-</edmx:Edmx>`);
+    res.send(xml);
+  } else {
+    res.status(500).send("WSDL query parameter is missing");
   }
 });
 
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send("Something broke!");
-});
-
-app.use((req, res, next) => {
-  console.log(`${new Date().toISOString()} - ${req.method} ${req.url}`);
-  next();
-});
-
 app.listen(port, () => {
-  console.log(`App listening on port ${port}`);
+  console.log(`SOAP Service running at port: ${port}`);
 });
