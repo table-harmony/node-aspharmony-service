37d339d4f73e4e929ff2a75b897bbeb8088b1129
table-harmony <lironkaner2007@gmail.com>
2024-10-01 11:33:47 +0300

    boop

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require("express");
+
 const app = express();
-const port = process.env.NODE_ENV === "development" ? 3000 : process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`App listening on port ${port}`);
 });
