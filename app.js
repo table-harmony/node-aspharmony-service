8125a192258c2d562f4d75374cf8d9035ec92c33
table-harmony <lironkaner2007@gmail.com>
2024-10-01 12:13:22 +0300

    xml

--- app.js
+++ app.js
@@ -4,7 +4,13 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 // Root endpoint
 app.get("/", (req, res) => {
@@ -25,37 +31,83 @@ app.get("/api", (req, res) => {
 
 // Service metadata endpoint
 app.get("/$metadata", (req, res) => {
-  res.json({
-    serviceName: "ASPHarmony",
-    version: "1.0.0",
-    description: "A simple API service for ASPHarmony",
-    endpoints: [
-      {
-        path: "/",
-        method: "GET",
-        description: "Service information",
-        responseFormat: {
-          service: "string",
-          version: "string",
-          endpoints: "array",
+  if (req.accepts("json")) {
+    res.json({
+      serviceName: "ASPHarmony",
+      version: "1.0.0",
+      description: "A simple API service for ASPHarmony",
+      endpoints: [
+        {
+          path: "/",
+          method: "GET",
+          description: "Service information",
+          responseFormat: {
+            service: "string",
+            version: "string",
+            endpoints: "array",
+          },
         },
-      },
-      {
-        path: "/api",
-        method: "GET",
-        description: "API root",
-        responseFormat: {
-          message: "string",
+        {
+          path: "/api",
+          method: "GET",
+          description: "API root",
+          responseFormat: {
+            message: "string",
+          },
         },
+      ],
+      dataTypes: {},
+      authenticationMethod: "None",
+      contactInformation: {
+        developer: "TableHarmony",
+        email: "contact@tableharmony.com",
       },
-    ],
-    dataTypes: {},
-    authenticationMethod: "None",
-    contactInformation: {
-      developer: "TableHarmony",
-      email: "tableharmony123@gmail.com",
-    },
-  });
+    });
+  } else {
+    res.type("application/xml");
+    res.send(`<?xml version="1.0" encoding="UTF-8"?>
+<edmx:Edmx xmlns:edmx="http://docs.oasis-open.org/odata/ns/edmx" Version="4.0">
+  <edmx:DataServices>
+    <Schema xmlns="http://docs.oasis-open.org/odata/ns/edm" Namespace="ASPHarmony.Models">
+      <EntityContainer Name="ASPHarmonyService">
+        <EntitySet Name="ServiceInfo" EntityType="ASPHarmony.Models.ServiceInfo">
+          <Annotation Term="OData.PublishingService">
+            <Record>
+              <PropertyValue Property="Name" String="ASPHarmony" />
+              <PropertyValue Property="Version" String="1.0.0" />
+            </Record>
+          </Annotation>
+        </EntitySet>
+        <FunctionImport Name="GetServiceInfo" ReturnType="ASPHarmony.Models.ServiceInfo" />
+        <FunctionImport Name="GetApiRoot" ReturnType="ASPHarmony.Models.ApiRoot" />
+      </EntityContainer>
+      <EntityType Name="ServiceInfo">
+        <Property Name="Service" Type="Edm.String" />
+        <Property Name="Version" Type="Edm.String" />
+        <Property Name="Endpoints" Type="Collection(ASPHarmony.Models.Endpoint)" />
+      </EntityType>
+      <EntityType Name="Endpoint">
+        <Property Name="Path" Type="Edm.String" />
+        <Property Name="Method" Type="Edm.String" />
+        <Property Name="Description" Type="Edm.String" />
+      </EntityType>
+      <EntityType Name="ApiRoot">
+        <Property Name="Message" Type="Edm.String" />
+      </EntityType>
+    </Schema>
+  </edmx:DataServices>
+</edmx:Edmx>`);
+  }
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
+
+app.use((req, res, next) => {
+  console.log(`${new Date().toISOString()} - ${req.method} ${req.url}`);
+  next();
 });
 
 app.listen(port, () => {
